e592e110ba859242a78dcfd9dbb3704a7e910b79
Roberta Vieira de Melo <roberta.vieirademelo@yahoo.com.br>
2023-07-12 16:53:24 -0300

    create gameCard, button

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Navbar from '@/components/navbar/navbar'
 import Subtitle from '@/components/tipography/subtitle/subtitle'
 import Container from '@/components/container/container'
 import SaleCard from '@/components/cards/saleCard/saleCard'
+import GameCard from '@/components/cards/gameCard/gameCard'
 
 
 export default function Home() {
@@ -31,6 +32,9 @@ export default function Home() {
 
           <div className={styles.session}>
           <Subtitle>Outros Jogos</Subtitle>
+          <div>
+            <GameCard />
+          </div>
           </div>
         </Container>
       </div>
